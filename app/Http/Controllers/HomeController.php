4f860dec318878dd8b5bd87968d8cf3fda5429d1
JeanBitencourtPro <jeanbitencourt.dev@outlook.com>
2020-08-14 20:22:39 -0300

    Criado a inserção da data da mensagem

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -14,12 +14,15 @@ class HomeController extends Controller
 
     public function enviar_mensagem()
     {
-        // var_dump($_REQUEST);die;
+        date_default_timezone_set('America/Sao_Paulo');
+        $date_message = date('Y-m-d H:i');
+
         $mensagem                   = new Mensagens();
         $mensagem->nome_completo    = $_REQUEST['nome'];
         $mensagem->telefone         = $_REQUEST['telefone'];
         $mensagem->email            = $_REQUEST['email'];
         $mensagem->mensagem         = $_REQUEST['mensagem'];
+        $mensagem->data_mensagem    = $date_message;
         $mensagem->save();
     }
 }
