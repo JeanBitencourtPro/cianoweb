fdbb06aab5cc1fc45030b1006d318b1c6ded2327
JeanBitencourtPro <jeanbitencourt.dev@outlook.com>
2020-09-04 10:25:27 -0300

    Atualização de metatags

--- resources/views/include/head.blade.php
+++ resources/views/include/head.blade.php
@@ -2,15 +2,18 @@
 <!-- Global site tag (gtag.js) - Google Analytics -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-175249118-1"></script>
     <script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
 
-    gtag('config', 'UA-175249118-1');
+        gtag('config', 'UA-175249118-1');
     </script>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="description" content="Ciano Web. Aumentando a visibilidade do seu negócio, com conteúdo visual para seus clientes, como chat-bot, blog's, sites, e-commerce e sistemas de gestão.">
+    <meta name="keywords" content="comunicação, chat-bot, integrações, redes sociais, blog, erp, e-commerce, site, api." />
+    <META NAME="Geography" CONTENT="Av Lúcio Bittencourt 1350 - Sapucaia do Sul, RS">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Ciano Web</title>
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400" rel="stylesheet" />
